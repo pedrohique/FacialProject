e7cf9eb8b5db8211febfa17a9e007f71553de1e3
pedro <pedrohique@hotmail.com>
2022-10-26 23:06:07 -0300

    Adicionado verificação de loguin para atualização

--- TestePyQT+Maquina.py
+++ TestePyQT+Maquina.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QWidget, QHBoxLayout
 import sys
@@ -49,7 +51,7 @@ class Janela(QWidget):
         atualizar.move(350, 250)  # define a posição do botão
         atualizar.resize(200, 150)  # Define o tamanho do botão
         atualizar.setStyleSheet('QPushButton {background-color: #f1f1f1; font:bold; font-size:20px}')  # estetica do botão
-        atualizar.clicked.connect(self.atualiza_json)
+        atualizar.clicked.connect(self.ConfereAtualiza)
 
         loguin = QPushButton('Loguin', self)
         loguin.move(100, 250)  # define a posição do botão
@@ -80,6 +82,7 @@ class Janela(QWidget):
         #iniciaJanela principal
 
         self.CarregarJanela()
+        self.atualiza_json()
 
     def atualiza_json(self):
 
@@ -91,23 +94,97 @@ class Janela(QWidget):
         data = {
             'siteid': siteid
         }
+        print(api_url_base + 'employeebio')
 
         re = requests.get(api_url_base + 'employeebio', params=data, headers=headers, verify=False)
+
 
         if re.reason == 'OK':
             bios = re.json()
             data_dict = {}
             for emp in bios['biometrics']:
                 if emp['FingerPrintTemplate']:
-                    data_dict[emp['ID']] = [list(emp['FingerPrintTemplate']), emp['BadgeNumber']]
+                    data_dict[emp['ID']] = [list(emp['FingerPrintTemplate']), emp['BadgeNumber'], emp['acess']]
                 else:
-                    data_dict[emp['ID']] = [None, emp['BadgeNumber']]
+                    data_dict[emp['ID']] = [None, emp['BadgeNumber'], emp['acess']]
             with open("template.json", "w") as arquivo:
                 json.dump(data_dict, arquivo, indent=4)
             self.label_1.setStyleSheet('QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: green}')
             self.label_1.setText('Biometrias atualizadas com sucesso.')
         else:
             print('Conexão APi falhou')
+
+
+    def ConfereAtualiza(self):
+        key = self.caixa_texto.text()
+        print(key)
+
+        def set_led_local(color=1, mode=3, speed=0x80, cycles=0):
+            """this is to make sure LED doesn't interfer with example
+            running on models without LED support - needs testing"""
+            try:
+                finger.set_led(color, mode, speed, cycles)
+            except Exception as exc:
+                print("INFO: Sensor les not support LED. Error:", str(exc))
+
+        with open("template.json", "r") as file:
+            data = json.load(file)
+            if key in data.keys():
+                if data[key][0]:
+                    print("Aguardando pela digital...")
+                    self.label_1.setText("Aguardando pela digital")
+                    set_led_local(color=3, mode=1)
+                    while finger.get_image() != adafruit_fingerprint.OK:
+                        pass
+                    print("Analisando...")
+                    self.label_1.setText("Analisando")
+
+                    if finger.image_2_tz(1) != adafruit_fingerprint.OK:
+                        pass
+
+                    finger.send_fpdata(data[key][0], "char", 2)
+                    i = finger.compare_templates()
+
+                    if i == adafruit_fingerprint.OK:
+                        i = finger.finger_search()
+                        set_led_local(color=2, speed=150, mode=6)
+
+
+
+                        print(f"Acesso liberado - Usuario: {key}  Aguardando Atualização")
+
+
+                        self.label_1.setStyleSheet(
+                            'QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: green}')
+                        self.label_1.setText(f"Acesso liberado - Usuario: {key} - Aguardando Atualização")
+                        time.sleep(1)
+                        self.atualiza_json()
+
+
+                    elif i == adafruit_fingerprint.NOMATCH:
+                        set_led_local(color=1, mode=2, speed=20, cycles=10)
+                        print(f"Digital não confere para o usuario: {key}")
+
+                        self.label_1.setStyleSheet(
+                            'QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: red}')
+                        self.label_1.setText(f"Digital não confere para o usuario: {key} - Não foi possivel atualizar")
+
+                        # atualiza_json(siteid)  # pendente
+                    else:
+                        print("Other error!")
+                        self.label_1.setStyleSheet(
+                            'QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: red}')
+                        self.label_1.setText("Other error!")
+                else:
+                    print('Digital não cadastrada.')
+                    self.label_1.setStyleSheet(
+                        'QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: red}')
+                    self.label_1.setText('Digital não cadastrada.')
+            else:
+                print('Usuario não cadastrado no sistema i9 procure um administrador')
+                self.label_1.setStyleSheet(
+                    'QLabel {background-color: #f1f1f1; font:bold; font-size:20px; color: red}')
+                self.label_1.setText('Usuario não cadastrado no sistema i9 procure um administrador')
 
 
     def ConfereDigital(self):
@@ -185,15 +262,6 @@ class Janela(QWidget):
         self.setWindowTitle(self.titulo)
         self.show()
 
-    def botao1_click(self):
-        conteudo = self.caixa_texto.text()
-
-        print(conteudo)
-        self.label_1.setStyleSheet('QLabel {background-color: blue; font:bold; font-size:20px}')
-
-    def botao2_click(self):
-        print('ola 2')
-        self.label_1.setStyleSheet('QLabel {background-color: red; font:bold; font-size:20px}')
 
 aplicacao = QApplication(sys.argv)
 j = Janela()
