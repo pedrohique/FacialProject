f3482847c06e402e6d3543d0788d6951bd681678
pedro <pedrohique@hotmail.com>
2022-11-13 18:57:13 -0300

    adicionado função que verifica se o arquivo config esta preenchido, caso não esteja ele pede os valores, usuario senha e siteid

--- maquina/executor.py
+++ maquina/executor.py
@@ -1,5 +1,6 @@
 import configparser
 import glob
+import time
 
 import requests
 import re
@@ -7,22 +8,45 @@ import re
 import os
 
 
-config = configparser.ConfigParser()
-config.read('config2.ini')
+# config = configparser.ConfigParser()
+# config.read('config2.ini')
 
 
-api_url_base = 'http://'+config.get('dados_api', 'server')+':'+config.get('dados_api', 'port') + '/'
+# api_url_base = 'http://'+config.get('dados_api', 'server')+':'+config.get('dados_api', 'port') + '/'
+#
+# dir_local = os.getcwd()
+#
+# loguin = {
+#     "username": config.get('dados_api', 'uid'),
+#     "password": config.get('dados_api', 'pwd'),
+#     "method": "OAuth2PasswordBearer",
+# }
 
-dir_local = os.getcwd()
 
-loguin = {
-    "username": config.get('dados_api', 'uid'),
-    "password": config.get('dados_api', 'pwd'),
-    "method": "OAuth2PasswordBearer",
-}
+def verifica_config(name_config):
+    config = configparser.ConfigParser()
+    config.read(name_config)
+    user = config.get('dados_api', 'uid')
+    password = config.get('dados_api', 'pwd')
+    siteid = config.get('dados_api', 'siteid')
+
+    if not siteid:
+        siteid = input('Digite o site id: ')
+        config.set('dados_api', 'siteid', siteid)
+    if not user:
+        user = input('Digite o nome de usuario: ')
+        config.set('dados_api', 'uid', user)
+    if not password:
+        password = input('Digite a senha: ')
+        config.set('dados_api', 'pwd', password)
+
+    with open(name_config, 'w') as configfile:
+        config.write(configfile)
+
+    return config
 
 
-def download_version(site, ver):
+def download_version(site, ver, dir_local, api_url_base, loguin):
     token = requests.post(api_url_base + 'token', data=loguin, verify=False)
 
     headers = {
@@ -54,7 +78,7 @@ def download_version(site, ver):
         return file
 
 
-def get_version():
+def get_version(dir_local):
     for i in glob.glob(dir_local + '\\version\\maquina_*'):
         versao = i.replace('.py', '')
         versao = str(versao.split('\\')[-1]).replace('maquina_', '')
@@ -63,11 +87,33 @@ def get_version():
 
 
 while True:
-    siteid = 'DEFAULT'
-    print('Buscando atualizações')
-    version = get_version()
-    arquivo = download_version(siteid, version)
+    # siteid = 'DEFAULT'
+    # print('Buscando atualizações')
+    config = verifica_config('config.ini')  # config =
+    user = config.get('dados_api', 'uid')
+    password = config.get('dados_api', 'pwd')
+    siteid = config.get('dados_api', 'siteid')
+
+
+    api_url_base = 'https://'+config.get('dados_api', 'server')+':'+config.get('dados_api', 'port') + '/'
+    print(api_url_base)
+
+    dir_local = os.getcwd()
+
+    loguin = {
+        "username": config.get('dados_api', 'uid'),
+        "password": config.get('dados_api', 'pwd'),
+        "method": "OAuth2PasswordBearer",
+    }
+
+    print(siteid, password, user)
+
+    version = get_version(dir_local)
+    print(version)
+    arquivo = download_version(siteid, version, dir_local, api_url_base, loguin)
+    print(arquivo)
     print('Abrindo sistema.')
+    # time.sleep(10)
     exec(open(arquivo).read())
 
 
